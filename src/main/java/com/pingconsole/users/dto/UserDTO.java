e5c05fee483d105f4d4dc84efa0abfbdc354d001
srv94 <saurabhverma42188@gmail.com>
2017-01-14 17:44:03 +0530

    f

---
+++ src/main/java/com/pingconsole/users/dto/UserDTO.java
@@ -0,0 +1,45 @@
+package com.pingconsole.users.dto;
+
+import java.util.Date;
+import java.util.List;
+
+public class UserDTO {
+	
+	private String userName;
+	private String name;
+	private String email;
+	private Date   dob;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public List<String> getRoles() {
+		return roles;
+	}
+	public void setRoles(List<String> roles) {
+		this.roles = roles;
+	}
+	private List<String> roles;
+	
+
+}
--- src/main/java/com/pingconsole/users/web/UsersController.java
+++ src/main/java/com/pingconsole/users/web/UsersController.java
@@ -1,14 +1,19 @@
 package com.pingconsole.users.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UsersController {
 	
+	private static String USER_INDEX_PAGE = "users/index";
+	
+	private static String ALL_USER_LIST = "userList";
+	
 	@RequestMapping("/users")
-	public String getRoleIndexPage(){
-		return "users/index";
+	public String getRoleIndexPage(Model model){
+		return USER_INDEX_PAGE;
 	}
 
 }
