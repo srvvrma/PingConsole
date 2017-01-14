38ca0d16bea0d47a51450b21def6e78735ad1d55
srv94 <saurabhverma42188@gmail.com>
2017-01-14 18:05:35 +0530

    Test Fail Change

--- src/test/java/com/pingconsole/PingConsoleApplicationTests.java
+++ src/test/java/com/pingconsole/PingConsoleApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nucleus;
+package com.pingconsole;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class NeoPingApplicationTests {
+public class PingConsoleApplicationTests {
 
 	@Test
 	public void contextLoads() {
