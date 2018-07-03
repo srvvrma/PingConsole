def70c7721eec9dc640c28ba5c2746fa8472a69f
saurabhverma <srvvrma0@gmail.com>
2018-07-03 22:42:28 +0530

    Upgrade Spring boot verions (1.*.* to 2.0) Remove Test case

--- src/test/java/com/pingconsole/RestTest.java
+++ src/test/java/com/pingconsole/RestTest.java
@@ -1,20 +1,12 @@
 package com.pingconsole;
 
 
-import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
-import static org.hamcrest.Matchers.*;
-
-import org.junit.Test;
-
-import io.restassured.response.Response;
-
 public class RestTest {
 	
-	@Test
+	/*@Test
 	public void givenU(){
 		Response r = get("http://jsonplaceholder.typicode.com/posts/1");
 		r.getBody().jsonPath().prettify();
-	}
+	}*/
 
 }
